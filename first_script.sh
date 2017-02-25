3456bc94f36bfe31898b3e8be02520437b6c2c86
Fernanda Valdovinos <Fernanda Valdovinos>
2017-02-25 12:21:15 -0700

    Adding help on how to run the script

--- first_script.sh
+++ first_script.sh
@@ -1,2 +1,7 @@
+if [ "$1" == "-h" ] || [ "$1" == "--h" ] ; then
+echo "Usage: sh $0 input_file ourput_file"
+exit
+fi
+
 # This script is used to count the number of characters in a file
 head -5 $1 | sort |wc -c > $2
